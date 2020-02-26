809e0ed45e56e761a25c8fc3c98682ba9c8047d9
Nick Kebbas <nkebbas@usfca.edu>
2020-02-26 12:19:41 -0800

    build d3 version of data

---
+++ d3/scatter-plot-matrix/chart.js
@@ -0,0 +1,107 @@
+/* Referenced the following scatterplot resources:
+
+https://observablehq.com/@d3/scatterplot-matrix
+https://bl.ocks.org/Fil/6d9de24b31cb870fed2e6178a120b17d
+
+*/
+var width = 960,
+    size = 230,
+    padding = 20;
+
+var x = d3.scaleLinear()
+    .range([padding / 2, size - padding / 2]);
+
+var y = d3.scaleLinear()
+    .range([size - padding / 2, padding / 2]);
+
+var xAxis = d3.axisBottom()
+    .scale(x)
+    .ticks(6);
+
+var yAxis = d3.axisLeft()
+    .scale(y)
+    .ticks(6);
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+d3.csv("filtered_data.csv").then(function(data) {
+
+  var domainByTrait = {},
+      measurements = d3.keys(data[0]).filter(function(d) { return d !== "name" && d !== "tier" && d !== "state"; }),
+      n = measurements.length;
+
+  measurements.forEach(function(measurement) {
+    domainByTrait[measurement] = d3.extent(data, function(d) {
+      return +d[measurement];
+    });
+    console.log(domainByTrait[measurement]);
+  });
+
+  xAxis.tickSize(size * n);
+  yAxis.tickSize(-size * n);
+
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", size * n + padding)
+      .attr("height", size * n + padding)
+    .append("g")
+      .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+      // placing the y axis
+  svg.selectAll(".x.axis")
+      .data(measurements)
+    .enter().append("g")
+      .attr("class", "x axis")
+      .attr("transform", function(d, i) { return "translate(" + (n - i - 1) * size + ",0)"; })
+      .each(function(d) { x.domain(domainByTrait[d]); d3.select(this).call(xAxis); });
+
+      // placing the y axis
+  svg.selectAll(".y.axis")
+      .data(measurements)
+    .enter().append("g")
+      .attr("class", "y axis")
+      .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
+      .each(function(d) { y.domain(domainByTrait[d]); d3.select(this).call(yAxis); });
+
+  var cell = svg.selectAll(".cell")
+      .data(cross(measurements, measurements))
+    .enter().append("g")
+      .attr("class", "cell")
+      .attr("transform", function(d) { return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")"; })
+      .each(plot);
+
+  // Titles for the diagonal.
+  cell.filter(function(d) { return d.i === d.j; }).append("text")
+      .attr("x", padding)
+      .attr("y", padding)
+      .attr("dy", ".71em")
+      .text(function(d) { return d.x; });
+
+
+  function plot(p) {
+    var cell = d3.select(this);
+
+    x.domain(domainByTrait[p.x]);
+    y.domain(domainByTrait[p.y]);
+
+    cell.append("rect")
+        .attr("class", "frame")
+        .attr("x", padding / 2)
+        .attr("y", padding / 2)
+        .attr("width", size - padding)
+        .attr("height", size - padding);
+
+// placing points and adding coloring here
+    cell.selectAll("circle")
+        .data(data)
+      .enter().append("circle")
+        .attr("cx", function(d) { return x(d[p.x]); })
+        .attr("cy", function(d) { return y(d[p.y]); })
+        .attr("r", 4)
+        .style("fill", function(d) { return color(d.tier); });
+  }});
+
+function cross(a, b) {
+  var c = [], n = a.length, m = b.length, i, j;
+  for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
+  return c;
+}
